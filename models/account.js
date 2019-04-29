3f6c63ba7c05de08fb0ad8aaba2dad49fcbfae79
Jay <jay@Macbook-Pro.home>
2019-04-29 01:33:01 -0400

    login fully functional

--- models/account.js
+++ models/account.js
@@ -1,49 +1,62 @@
-var AWS = require('aws-sdk')
-AWS.config.loadFromPath('./credentials.json')
+var AWS = require("aws-sdk");
+AWS.config.loadFromPath("./credentials.json");
+
+const errorMessage = "Error: ";
+const emailNotFoundMessage = "Email not found";
+const wrongPasswordMessage = "Wrong password";
+const loginSuccessMessage = "Login success";
 
 var documentClient = new AWS.DynamoDB.DocumentClient();
 
 function isEmpty(obj) {
-    for(var key in obj) {
-        if(obj.hasOwnProperty(key))
-            return false;
-    }
-    return true;
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) return false;
+  }
+  return true;
 }
 
 module.exports = {
-    login(email_input, password_input) {
+  async login(email_input, password_input, returnObject, callback) {
+    var params = {
+      TableName: "Account",
+      Key: {
+        email: email_input
+      }
+    };
 
-        var params = {
-            TableName: 'Account',
-            Key: {
-                email : email_input
-            }
+    await documentClient.get(params, function(err, data) {
+      if (err) {
+        console.log(
+          `Error occurred during login() with email ${email_input}. `,
+          err
+        );
+        returnObject.message = errorMessage + err;
+      } else {
+        if (isEmpty(data)) {
+          console.log(`couldn't find user with email ${email_input}`);
+          returnObject.message = emailNotFoundMessage;
+        } else {
+          var password = data["Item"].password;
+
+          if (password === password_input) {
+            console.log("login successful");
+            returnObject.message = loginSuccessMessage;
+            returnObject.data = data;
+            returnObject.successful = true;
+          } else {
+            console.log("wrong password");
+            returnObject.message = wrongPasswordMessage;
+          }
         }
+      }
 
-        documentClient.get(params, function(err, data) {
-            if (err) {
-                console.log(`Error occurred during login() with email ${email_input}. `, err)
-            }
+      callback()
 
-            console.log(data)
+    });
 
-            if (isEmpty(data)) {
-                return console.log(`couldn't find user with email ${email_input}`)
-            } else {
+  },
+  signup(new_email, new_password) {
+      
+  }
 
-                var password = data['Item'].password
-
-                if (password === password_input) {
-                    console.log('login successful')
-                } else {
-                    console.log('wrong password')
-                }
-            }
-
-        })
-    },
-    signup(new_email, new_password) {
-
-    }
-}
+};
--- server.js
+++ server.js
@@ -4,17 +4,24 @@ const account = require('./models/account.js')
 const express = require('express')
 
 var app = express()
-var test_email = 'tessdfting'
-var test_password = 'testing'
 
 app.get('/', function(req, res) {
     res.send('server is running')
 })
 
 app.get('/login/:email/:password', function(req, res){
-    console.log(req.params)
-    account.login(req.params.email, req.params.password)
-    res.send(res)
+    var returnObject = new Object()
+
+    account.login(req.params.email, req.params.password, returnObject, function() {
+        console.log(returnObject)
+        res.send(returnObject)
+    })
+
+})
+
+app.get('/signup', function(req, res) {
+    account.signup(1, 1)
+    console.log(2)
 })
 
 var server = app.listen(3000, function() {
